d3169f2cc6e1a017ee59c9786076292fefb38063
Kasuma <tony.proeung@gmail.com>
2020-06-15 19:13:38 -0400

    Navbar/Article Work

---
+++ application/views/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <?php include "./head.html"; ?>
+    <title>The Den</title>
+  </head>
+  <body>
+    <div id="navbar">
+      <?php include "./navbar.html"; ?>
+    </div>
+    <div class="container">
+      <div id="latest_article1">
+        <h3>Latest Article!</h3>
+        <h6><em>Current Date</em></h6>
+        <p>Your immediate attention is needed, Arks! The Mothership has been corrupted and is moving to intercept the main fleet of Oracle. As this course runs countermand to previous navigational orders, it is believed that the helm has been taken over by Dark Falz and his minions. Operatives are to prepare for an all-out attempt to retake what is ours, eradicate all Falspawn, and locate Dark Falz.</p>
+      </div>
+      <div id="latest_article2">
+        <h3>Latest Article!</h3>
+        <h6><em>Current Date</em></h6>
+        <p>Your immediate attention is needed, Arks! The Mothership has been corrupted and is moving to intercept the main fleet of Oracle. As this course runs countermand to previous navigational orders, it is believed that the helm has been taken over by Dark Falz and his minions. Operatives are to prepare for an all-out attempt to retake what is ours, eradicate all Falspawn, and locate Dark Falz.</p>
+      </div>
+    </div>
+    <div id="like_button_container"></div>
+
+    <?php include "./scripts.html"; ?>
+  </body>
+</html>
