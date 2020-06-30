d32a71c4cdd1016e9d3caa5d07a287c7901acf08
Kasuma <tony.proeung@gmail.com>
2020-06-30 00:47:30 -0400

    Img Wrapper Complete!

---
+++ application/views/components/image_wrapper.js
@@ -0,0 +1,50 @@
+import "./image_wrapper.css"; //Imports CSS file for component
+
+class ImgWrapper extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            hover: false,
+            message: "",
+            css_class: "second_class"
+        };
+        this.hoverOn = this.hoverOn.bind(this);
+        this.hoverOff = this.hoverOff.bind(this);
+    }
+    
+    hoverOn() {
+        console.log("Hover On!")
+        this.setState({
+            hover: true,
+        })
+        
+        console.log(this.state);
+    }
+
+    hoverOff() {
+        console.log("Hover Off!")
+        this.setState({
+            hover: false,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <img className={this.state.css_class} src={ this.props.url } onMouseOver={ this.hoverOn } onMouseLeave={ this.hoverOff } />
+                    <div className="overlay">
+                      <div className="center_text">{ this.props.text } </div>
+                    </div>
+                </div>
+            </div>            
+        );
+    }
+}
+
+const ImageContainer_1 = document.querySelector("#image_category1");
+const ImageContainer_2 = document.querySelector("#image_category2");
+const ImageContainer_3 = document.querySelector("#image_category3");
+ReactDOM.render(<ImgWrapper url={"./cityscape.jpg"} text={'LIFE'} />, ImageContainer_1);
+ReactDOM.render(<ImgWrapper url={"./food.jpg"} text={'FOOD'} />, ImageContainer_2);
+ReactDOM.render(<ImgWrapper url={"./vgcontrollers.jpg"} text={'GAMES'} />, ImageContainer_3);
--- application/views/index.php
+++ application/views/index.php
@@ -11,21 +11,30 @@
       <?php include "./navbar.html"; ?>
     </div>
     <div class="container">
-      <div id="latest_article1">
-        <h3>Latest Article!</h3>
-        <h6><em>Current Date</em></h6>
-        <p>Your immediate attention is needed, Arks! The Mothership has been corrupted and is moving to intercept the main fleet of Oracle. As this course runs countermand to previous navigational orders, it is believed that the helm has been taken over by Dark Falz and his minions. Operatives are to prepare for an all-out attempt to retake what is ours, eradicate all Falspawn, and locate Dark Falz.</p>
+      <div class="row">
+        <div id="latest_article1">
+          <h3>Latest Article!</h3>
+          <h6><em>Current Date</em></h6>
+          <p>Your immediate attention is needed, Arks! The Mothership has been corrupted and is moving to intercept the main fleet of Oracle. As this course runs countermand to previous navigational orders, it is believed that the helm has been taken over by Dark Falz and his minions. Operatives are to prepare for an all-out attempt to retake what is ours, eradicate all Falspawn, and locate Dark Falz.</p>
+        </div>
       </div>
-      <div id="latest_article2">
-        <h3>Latest Article!</h3>
-        <h6><em>Current Date</em></h6>
-        <p>Your immediate attention is needed, Arks! The Mothership has been corrupted and is moving to intercept the main fleet of Oracle. As this course runs countermand to previous navigational orders, it is believed that the helm has been taken over by Dark Falz and his minions. Operatives are to prepare for an all-out attempt to retake what is ours, eradicate all Falspawn, and locate Dark Falz.</p>
+      <div class="row">
+        <div id="latest_article2">
+          <h3>Latest Article!</h3>
+          <h6><em>Current Date</em></h6>
+          <p>Your immediate attention is needed, Arks! The Mothership has been corrupted and is moving to intercept the main fleet of Oracle. As this course runs countermand to previous navigational orders, it is believed that the helm has been taken over by Dark Falz and his minions. Operatives are to prepare for an all-out attempt to retake what is ours, eradicate all Falspawn, and locate Dark Falz.</p>
+        </div>
       </div>
-    </div>
-    <div id="like_button_container"></div>
-    <div class="container" id="article_categories">
-      <div id="life_image">
-        <img src="./components/cityscape.jpg" alt="A cityscape">
+      <div class="row" id="article_categories">
+        <div class="col-12 col-md">
+          <div id="image_category1"></div>
+        </div>
+        <div class="col-12 col-md">
+          <div id="image_category2"></div>
+        </div>
+        <div class="col-12 col-md">
+          <div id="image_category3"></div>
+        </div>
       </div>
     </div>
 
