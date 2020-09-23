c727297af267313fd8071d2bc3e53c8a916dcfdb
Kasuma <tony.proeung@gmail.com>
2020-09-23 15:22:10 -0400

    Fixed article view component

--- application/views/components/article_container.jsx
+++ application/views/components/article_container.jsx
@@ -1,22 +1,20 @@
-function createMarkup() {
-    return {__html: `<p>${this.props.article.articleContent}</p>`};
-}
-
-function setHTML() {
-    return <div dangerouslySetInnerHTML={createMarkup()}></div>;
-}
-
 class ArticleDisplay extends React.Component{
     constructor(props){
         super(props);
 
     }
+    
+    rawMarkup(){
+        var markup = this.props.article.articleContent;
+        return { __html: markup };
+    }
+
     render() {
         return (
             <div>
                 <h3>{this.props.article.articleName}</h3>
                 <h6>{this.props.article.articleDate}</h6>
-                {setHTML()}
+                <p><span dangerouslySetInnerHTML={this.rawMarkup()} /></p>
             </div>
         );
     }
